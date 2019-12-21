20711113fd7181b3854491666bcfd61989ef5fd8
ANANDA RIZKY Y <kiky.lenovo@gmail.com>
2019-12-21 22:02:42 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  state = {
+    data: []
+  }
+
+  fetchData = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+      .then(res => {
+        this.setState({
+          data: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  
+  render(){
+    return(
+      <div className="container">
+        <table>
+          <tr>
+            <th>Nomor</th>
+            <th>Nama Perintah</th>
+            <th>Apakah Selesai ?</th>
+          </tr>
+          {this.state.data.map((item, index) => {
+          let isCompleted = item.completed ? "Sudah selesai" : "Belum selesai";
+  
+          if (item.id < 10){
+            return(
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{isCompleted}</td>
+              </tr>
+            )
+          }
+        })}
+
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
